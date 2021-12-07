96155354fccb882db4167586324306dd719a014a
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-07 14:39:30 -0600

    categories and spoons now appear on page

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { getAllTasks } from "./TaskManager";
-import { Card } from "react-bootstrap";
+import { getAllCategories, getAllSpoons, getAllTasks } from "./TaskManager";
+import { Card, Form } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 
@@ -16,8 +16,40 @@ export const TaskList = () => {
     useEffect(() =>{
         taskFetcher()
     }, [])
+
+    useEffect(() =>{
+        getAllCategories().then(data=>setCategories(data))
+    }, [])
+
+    useEffect(() =>{
+        getAllSpoons().then(data=>setSpoons(data))
+    }, [])
+
+    
+   
     return (
         <>
+        {/* Top of page, filtering options */}
+        <Form>
+            <Form.Label>Filter by Category</Form.Label>
+            {
+                categories.map(cat => {
+                    return <Form.Check>
+                        {cat.label}
+                    </Form.Check>
+                })
+            }
+            <Form.Label>Filter by amount of spoons</Form.Label>
+            <Form.Select>
+            <option>Select a number of spoons</option>
+            {
+                spoons.map(spoon => {
+                    return <option value={spoon.id}>{spoon.number_of_spoons}</option>
+                })
+            }
+            </Form.Select>
+        </Form>
+
         <Row xs='auto'md='auto' lg='auto'>
             {
                 tasks.map(task => {
