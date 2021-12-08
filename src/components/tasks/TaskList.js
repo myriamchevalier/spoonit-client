dab0feb790209d693aa362ef878cfad33ba0b89f
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-08 09:43:53 -0600

    filtering implemented

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,69 +1,86 @@
 import React, { useEffect, useState } from "react";
-import { getAllCategories, getAllSpoons, getAllTasks } from "./TaskManager";
-import { Card, Form } from "react-bootstrap";
-import { Row } from "react-bootstrap";
+import { getAllCategories, getAllSpoons, getTasks } from "./TaskManager";
+import { Button, Card, Form, Row } from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 
 export const TaskList = () => {
-    const [ tasks, setTasks ] = useState([])
-    const [ categories, setCategories] = useState([])
-    const [ spoons, setSpoons] = useState([])
-
+    const [tasks, setTasks] = useState([])
+    const [categories, setCategories] = useState([])
+    const [spoons, setSpoons] = useState([])
+    const [params, setParams] = useState({}) // state to contain filter parameters.
+    
     const taskFetcher = () => {
-        getAllTasks().then(data=>setTasks(data))
+        getTasks(params).then(data => {setTasks(data)}) // passing params as an argument.
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         taskFetcher()
+        getAllCategories().then(data => setCategories(data))
+        getAllSpoons().then(data => setSpoons(data))
     }, [])
 
-    useEffect(() =>{
-        getAllCategories().then(data=>setCategories(data))
-    }, [])
-
-    useEffect(() =>{
-        getAllSpoons().then(data=>setSpoons(data))
-    }, [])
-
-    
-   
     return (
         <>
-        {/* Top of page, filtering options */}
-        <Form>
-            <Form.Label>Filter by Category</Form.Label>
-            {
-                categories.map(cat => {
-                    return <Form.Check>
-                        {cat.label}
-                    </Form.Check>
-                })
-            }
-            <Form.Label>Filter by amount of spoons</Form.Label>
-            <Form.Select>
-            <option>Select a number of spoons</option>
-            {
-                spoons.map(spoon => {
-                    return <option value={spoon.id}>{spoon.number_of_spoons}</option>
-                })
-            }
-            </Form.Select>
-        </Form>
+            {/* Top of page, filtering options */}
+            <Form>
+                <Form.Label>Filter by Category</Form.Label>
+                <Form.Select onChange={(e) => { 
+                    const value = e.currentTarget.value // grabbing the value of what user selects
+                    const updatedParams = { ...params } // making a copy of the params state
+                    if (value) {
+                        updatedParams.categoryId = value // if user selects a category, value added to copy of state
+                    } else {
+                        delete updatedParams.categoryId // if user unselects a category, delete categoryId from the copy of state
+                    }
+                    setParams(updatedParams) // No matter what, params state gets updated with copy of state.
+                }}>
+                    <option value="" >Select a category</option>
+                    {
+                        categories.map(cat => {
+                            return <option value={cat.id}>{cat.label}</option>
+                        })
+                    }
+                </Form.Select>
 
-        <Row xs='auto'md='auto' lg='auto'>
-            {
-                tasks.map(task => {
-                    return <Card style={{ width: '18rem', margin: '2rem' }}>
-                        <CardHeader>{task.category?.label}</CardHeader>
-                        <Card.Body key={task.id}>
-                            <Card.Title>{task.name}</Card.Title>
-                            <Card.Text>{task.description}</Card.Text>
-                            <Card.Text>Spoons: {task.spoon?.number_of_spoons}</Card.Text>
-                        </Card.Body>
-                    </Card>
-                })
-            }
-        </Row>
+                <Form.Label>Filter by amount of spoons</Form.Label>
+                <Form.Select onChange={(e) => {
+                    const value = e.currentTarget.value
+                    const updatedParams = { ...params }
+                    if (value) {
+                        updatedParams.spoonId = value
+                    } else {
+                        delete updatedParams.spoonId
+                    }
+                    setParams(updatedParams)
+                }
+
+                }>
+                    <option value="">Select a number of spoons</option>
+                    {
+                        spoons.map(spoon => {
+                            return <option value={spoon.id}>{spoon.number_of_spoons}</option>
+                        })
+                    }
+                </Form.Select>
+                <Button onClick={taskFetcher}>Apply filters</Button>
+            </Form>
+
+            <Row xs='auto' md='auto' lg='auto'>
+                {
+                    tasks.length === 0 ?
+                    <p>There are no results to be displayed</p> :
+                    tasks.map(task => {
+                        return <Card key={task.id} style={{ width: '18rem', margin: '2rem' }}>
+                            <CardHeader>{task.category?.label}</CardHeader>
+                            <Card.Body key={task.id}>
+                                <Card.Title>{task.name}</Card.Title>
+                                <Card.Text>{task.description}</Card.Text>
+                                <Card.Text>Spoons: {task.spoon?.number_of_spoons}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    })
+                }
+            </Row>
         </>
     )
 }
