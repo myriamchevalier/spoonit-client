bdc7430177a658b580b21a95289e60a2e6578562
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-09 11:26:59 -0600

    partial form

---
+++ src/components/tasks/TaskForm.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { getAllCategories, getAllSpoons } from "./TaskManager";
+
+
+export const TaskForm = () => {
+    const [categories, setCategories] = useState([])
+    const [spoons, setSpoons] = useState([])
+    const [task, setTask] = useState({})
+
+    useEffect(() => {
+        getAllCategories().then(data => setCategories(data))
+        getAllSpoons().then(data => setSpoons(data))
+    }, [])
+
+    return (
+        <>
+            <form>
+                <h1>Create New Task</h1>
+                
+                <fieldset>
+                    <div>
+                        <label htmlFor='name'>Name</label>
+                        <input type='text' name='name' placeholder='Task Name'/>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <div className='form-group'>
+                        <label htmlFor='categoryId'>Category</label>
+                        <select name='categoryId' onChange={handleControlledInputChange}>
+                    <option value="" >Select a category</option>
+                    {
+                        categories.map(cat => {
+                            return <option value={cat.id} key={cat.id}>{cat.label}</option>
+                        })
+                    }
+                        </select>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <div className='form-group'>
+                        <label htmlFor='spoonId'>Spoons</label>
+                        <select name='spoonId' onChange={handleControlledInputChange}>
+                    <option value="" >Select an amount of spoons</option>
+                    {
+                        spoons.map(spoon => {
+                            return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
+                        })
+                    }
+                        </select>
+                    </div>
+                </fieldset>
+            </form>
+ 
+        </>
+    )
+}
+'category', 'spoon', 'description'
