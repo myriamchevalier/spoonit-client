1b628b1fd3e290c0a440d4a54f0cb8ffb86a50d5
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-15 11:42:45 -0600

    styled randomizer and task list pages

--- src/components/tasks/Randomizer.js
+++ src/components/tasks/Randomizer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { getRandomTask, getAllCategories, getAllSpoons } from "./TaskManager";
+import "./Randomizer.css"
 
 export const Randomizer = () => {
     const [randomTask, setRandomTask] = useState([])
@@ -10,7 +11,7 @@ export const Randomizer = () => {
     const [newRoll, setNewRoll] = useState(false)
 
     const randomTaskFetcher = () => {
-        getRandomTask(params).then(data => {setRandomTask(data)}) // passing params as an argument.
+        getRandomTask(params).then(data => { setRandomTask(data) }) // passing params as an argument.
     }
 
     useEffect(() => {
@@ -33,7 +34,7 @@ export const Randomizer = () => {
     const handleNewRoll = (e) => {
         e.preventDefault()
         newRoll ? setNewRoll(false) :
-        setNewRoll(true)
+            setNewRoll(true)
     }
 
     useEffect(() => {
@@ -42,18 +43,19 @@ export const Randomizer = () => {
 
     return <>
         <h1>SpoonIt!</h1>
-                    {/* Top of page, filtering options */}
-            <form>
+
+        <article className="rand-container">
+            <form className="rand-filters">
                 <fieldset>
                     <div className='form-group'>
                         <label htmlFor='categoryId'>Filter by Category</label>
                         <select name='categoryId' onChange={handleControlledInputChange}>
-                    <option value="" >Select a category</option>
-                    {
-                        categories.map(cat => {
-                            return <option value={cat.id} key={cat.id}>{cat.label}</option>
-                        })
-                    }
+                            <option value="" >Select a category</option>
+                            {
+                                categories.map(cat => {
+                                    return <option value={cat.id} key={cat.id}>{cat.label}</option>
+                                })
+                            }
                         </select>
                     </div>
                 </fieldset>
@@ -62,12 +64,12 @@ export const Randomizer = () => {
                     <div className='form-group'>
                         <label htmlFor='spoonId'>Filter by Spoons</label>
                         <select name='spoonId' onChange={handleControlledInputChange}>
-                    <option value="" >Select an amount of spoons</option>
-                    {
-                        spoons.map(spoon => {
-                            return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
-                        })
-                    }
+                            <option value="" >Select an amount of spoons</option>
+                            {
+                                spoons.map(spoon => {
+                                    return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
+                                })
+                            }
                         </select>
                     </div>
                 </fieldset>
@@ -75,35 +77,38 @@ export const Randomizer = () => {
                 <fieldset>
                     <div className='form-group'>
                         <label htmlFor='createdBy'>What view would you prefer?</label>
-                        <input type='radio' name='createdBy' value='' onChange={handleControlledInputChange}/> All Tasks
-                        <input type='radio' name='createdBy' value='tr' onChange={handleControlledInputChange}/> My Tasks Only
-                     
+                        <input type='radio' name='createdBy' value='' onChange={handleControlledInputChange} /> All Tasks
+                        <input type='radio' name='createdBy' value='tr' onChange={handleControlledInputChange} /> My Tasks Only
+
                     </div>
                 </fieldset>
-        
-                <button onClick={(event)=> {
-                event.preventDefault() 
-                randomTaskFetcher()}}>
-                Apply filters
-                </button>
-                    
-            </form>
 
-            <div>
-                {randomTask ?
-                <Card key={randomTask.id} style={{ width: '18rem', margin: '2rem' }}>
-                    <Card.Header>{randomTask.category?.label}</Card.Header>
-                    <Card.Body key={randomTask.id}>
-                        <Card.Title>{randomTask.name}</Card.Title>
-                        <Card.Text>{randomTask.description}</Card.Text>
-                        <Card.Text>Spoons: {randomTask.spoon?.number_of_spoons}</Card.Text>
-                    </Card.Body>
-                    
-                </Card> :
-                <p>No result to be displayed</p>}
-            </div>
-            <div>
-            <button onClick={handleNewRoll}>Roll New Task!</button>
-            </div>        
+                <button onClick={(event) => {
+                    event.preventDefault()
+                    randomTaskFetcher()
+                }}>
+                    Apply filters
+                </button>
+
+            </form>
+            <section className="rand-task-container">
+                <div>
+                    {randomTask ?
+                        <Card key={randomTask.id} style={{ width: '18rem', margin: '2rem' }}>
+                            <Card.Header>{randomTask.category?.label}</Card.Header>
+                            <Card.Body key={randomTask.id}>
+                                <Card.Title>{randomTask.name}</Card.Title>
+                                <Card.Text>{randomTask.description}</Card.Text>
+                                <Card.Text>Spoons: {randomTask.spoon?.number_of_spoons}</Card.Text>
+                            </Card.Body>
+
+                        </Card> :
+                        <p>No result to be displayed</p>}
+                </div>
+                <div>
+                    <button onClick={handleNewRoll}>Roll New Task!</button>
+                </div>
+            </section>
+        </article>
     </>
 }
--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { deleteTask, getAllCategories, getAllSpoons, getTasks } from "./TaskManager";
-import { Card, Row} from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import { useHistory } from "react-router";
+import "./TaskList.css"
 
 export const TaskList = () => {
     const [tasks, setTasks] = useState([])
@@ -10,9 +11,9 @@ export const TaskList = () => {
     const [spoons, setSpoons] = useState([])
     const [params, setParams] = useState({}) // state to contain filter parameters.
     const history = useHistory()
-    
+
     const taskFetcher = () => {
-        getTasks(params).then(data => {setTasks(data)}) // passing params as an argument.
+        getTasks(params).then(data => { setTasks(data) }) // passing params as an argument.
     }
 
     useEffect(() => {
@@ -39,73 +40,81 @@ export const TaskList = () => {
 
     return (
         <>
-            {/* Top of page, filtering options */}
-            <form>
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='categoryId'>Filter by Category</label>
-                        <select name='categoryId' onChange={handleControlledInputChange}>
-                            <option value="" >Select a category</option>
-                            {
-                                categories.map(cat => {
-                                    return <option value={cat.id} key={cat.id}>{cat.label}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
+            <article className="tasklist-container">
+                {/* Top of page, filtering options */}
+                <form className="filters">
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='categoryId'>Filter by Category</label>
+                            <select name='categoryId' onChange={handleControlledInputChange}>
+                                <option value="" >Select a category</option>
+                                {
+                                    categories.map(cat => {
+                                        return <option value={cat.id} key={cat.id}>{cat.label}</option>
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </fieldset>
 
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='spoonId'>Filter by Spoons</label>
-                        <select name='spoonId' onChange={handleControlledInputChange}>
-                            <option value="" >Select an amount of spoons</option>
-                            {
-                                spoons.map(spoon => {
-                                    return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                </fieldset>
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='spoonId'>Filter by Spoons</label>
+                            <select name='spoonId' onChange={handleControlledInputChange}>
+                                <option value="" >Select an amount of spoons</option>
+                                {
+                                    spoons.map(spoon => {
+                                        return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </fieldset>
 
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='createdBy'>What view would you prefer?</label>
-                        <input type='radio' name='createdBy' value='' onChange={handleControlledInputChange}/> All Tasks
-                        <input type='radio' name='createdBy' value='tr' onChange={handleControlledInputChange}/> My Tasks Only
-                     
-                    </div>
-                </fieldset>
-        
-                <button onClick={(event)=> {
-                event.preventDefault() 
-                taskFetcher()
-                }}>Apply filters</button>
-                    
-            </form>
+                    <fieldset>
+                        <div className="toggle-view">
+                            <p> <input type='radio' name='createdBy' value='' onChange={handleControlledInputChange} /> All Tasks</p>
+                            <p> <input type='radio' name='createdBy' value='tr' onChange={handleControlledInputChange} /> My Tasks Only </p>
 
-            <Row xs='auto' md='auto' lg='auto'>
-                {
-                    tasks.length === 0 ?
-                    <p>There are no results to be displayed</p> :
-                    tasks.map(task => {
-                        return <Card key={task.id} style={{ width: '18rem', margin: '2rem' }}>
-                            <CardHeader>{task.category?.label}</CardHeader>
-                            <Card.Body key={task.id}>
-                                <Card.Title>{task.name}</Card.Title>
-                                <Card.Text>{task.description}</Card.Text>
-                                <Card.Text>Spoons: {task.spoon?.number_of_spoons}</Card.Text>
-                                {task.is_universal ? " " : <button onClick={()=> {
-                                history.push({ pathname: `/tasks/edit/${task.id}`})}}>Edit</button>} 
-                                {task.is_universal ? "" : <button onClick={()=> 
-                                deleteTask(task.id)
-                                .then(onDelete(task.id))}>Delete</button>}
-                            </Card.Body>
-                        </Card>
-                    })
-                }
-            </Row>
+                        </div>
+                    </fieldset>
+
+                    <button onClick={(event) => {
+                        event.preventDefault()
+                        taskFetcher()
+                    }}>Apply filters</button>
+
+                    <div className="add-task">
+                        <button onClick={() => {
+                            history.push({ pathname: `/tasks/form` })
+                        }}>Add a new task</button>
+                    </div>
+
+                </form>
+
+                <Row xs='auto' md='auto' lg='auto'>
+                    {
+                        tasks.length === 0 ?
+                            <p>There are no results to be displayed</p> :
+                            tasks.map(task => {
+                                return <Card key={task.id} style={{ width: '18rem', margin: '2rem' }}>
+                                    <CardHeader>{task.category?.label}</CardHeader>
+                                    <Card.Body key={task.id}>
+                                        <Card.Title>{task.name}</Card.Title>
+                                        <Card.Text>{task.description}</Card.Text>
+                                        <Card.Text>Spoons: {task.spoon?.number_of_spoons}</Card.Text>
+                                        {task.is_universal ? " " : <button onClick={() => {
+                                            history.push({ pathname: `/tasks/edit/${task.id}` })
+                                        }}>Edit</button>}
+                                        {task.is_universal ? "" : <button onClick={() =>
+                                            deleteTask(task.id)
+                                                .then(onDelete(task.id))}>Delete</button>}
+                                    </Card.Body>
+                                </Card>
+                            })
+                    }
+                </Row>
+            </article>
         </>
     )
 }
