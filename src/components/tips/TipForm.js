3bd9aae8421a44144df215af753a0751ce6e541a
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-16 11:25:50 -0600

    minimal styling added to tips

--- src/components/tips/TipForm.js
+++ src/components/tips/TipForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { createNewTip, getSingleTip, getTopics, updateTip } from "./TipManager";
+import "./TipForm.css"
 
 export const TipForm = () => {
     const [topics, setTopics] = useState([])
@@ -31,12 +32,12 @@ export const TipForm = () => {
 
     return (
         <>
-            <form>
+        <div className="form-container">
+            <form className="tip-form">
                 <h3>{tipId ? 'Edit tip' : 'Create new tip'}</h3>
 
                 <fieldset>
                     <div className='form-group-tip'>
-                        <label htmlFor='topicId'>Topic</label>
                         <select name='topicId' value={tip.topicId} required
                         onChange={handleControlledInputChange}>
                             <option value=''>Select a topic</option>
@@ -52,18 +53,19 @@ export const TipForm = () => {
                 <fieldset>
                     <div>
                         <label htmlFor='content'>
-                            <input type='text' required name='content' placeholder='Your tip here'
+                            <textarea required name='content' placeholder='Your tip here'
                             value={tip.content} onChange={handleControlledInputChange} />
                         </label>
                     </div>
                 </fieldset>
-            </form>
-            
             <button onClick={e => {
                 e.preventDefault()
                 tipId? updateTip(tip).then(() => history.push('/tips')) :
                 createNewTip(tip).then(() => history.push('/tips'))
             }}>Save Tip</button>
+            </form>
+            
+        </div>
         </>
     )
 }
