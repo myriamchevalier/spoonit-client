d9b6178c163d0dcd4d0a3f2b66a89c48b11af7b2
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-12-15 21:17:50 -0600

    minimal styling on create task

--- src/components/tasks/TaskForm.js
+++ src/components/tasks/TaskForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { getAllCategories, getAllSpoons, createNewTask, getSingleTask, updateTask } from "./TaskManager";
-
+import "./TaskForm.css"
 
 export const TaskForm = () => {
     const [categories, setCategories] = useState([])
@@ -20,78 +20,83 @@ export const TaskForm = () => {
             getSingleTask(taskId).then((taskData) => {
                 setTask(
                     // Unpack response
-                    {...taskData,
-                    categoryId: taskData.category.id,
-                    spoonId: taskData.spoon.id}
+                    {
+                        ...taskData,
+                        categoryId: taskData.category.id,
+                        spoonId: taskData.spoon.id
+                    }
                 )
             })
         }
-    },[taskId])
+    }, [taskId])
 
     const handleControlledInputChange = (e) => {
-        const newTask = {...task}
+        const newTask = { ...task }
         newTask[e.target.name] = e.target.value
         setTask(newTask)
     }
 
     return (
         <>
-            <form>
-                <h2>{taskId ? 'Update a task': 'Create new task'}</h2>
-                
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='name'>Name</label>
-                        <input type='text' name='name' placeholder='Task Name' 
-                        value={task.name} onChange={handleControlledInputChange}/>
-                    </div>
-                </fieldset>
+            <div className="form-container">
+                <form className="task-form">
+                    <h2>{taskId ? 'Update a task' : 'Create new task'}</h2>
 
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='categoryId'>Category</label>
-                        <select name='categoryId' value={task.categoryId} 
-                        onChange={handleControlledInputChange}>
-                    <option value="" >Select a category</option>
-                    {
-                        categories.map(cat => {
-                            return <option value={cat.id} key={cat.id}>{cat.label}</option>
-                        })
-                    }
-                        </select>
-                    </div>
-                </fieldset>
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' name='name' placeholder='Task Name'
+                                value={task.name} onChange={handleControlledInputChange} />
+                        </div>
+                    </fieldset>
 
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='spoonId'>Spoons</label>
-                        <select name='spoonId' value={task.spoonId} 
-                        onChange={handleControlledInputChange}>
-                    <option value="" >Select an amount of spoons</option>
-                    {
-                        spoons.map(spoon => {
-                            return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
-                        })
-                    }
-                        </select>
-                    </div>
-                </fieldset>
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='categoryId'>Category</label>
+                            <select name='categoryId' value={task.categoryId}
+                                onChange={handleControlledInputChange}>
+                                <option value="" >Select a category</option>
+                                {
+                                    categories.map(cat => {
+                                        return <option value={cat.id} key={cat.id}>{cat.label}</option>
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </fieldset>
 
-                <fieldset>
-                    <div className='form-group'>
-                        <label htmlFor='description'>Description</label>
-                        <input type='textarea' name='description' placeholder='Task Description' 
-                        value={task.description} onChange={handleControlledInputChange}/>
-                    </div>
-                </fieldset>
-            </form>
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='spoonId'>Spoons</label>
+                            <select name='spoonId' value={task.spoonId}
+                                onChange={handleControlledInputChange}>
+                                <option value="" >Select an amount of spoons</option>
+                                {
+                                    spoons.map(spoon => {
+                                        return <option value={spoon.id} key={spoon.id}>{spoon.number_of_spoons}</option>
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </fieldset>
 
+                    <fieldset>
+                        <div className='form-group'>
+                            <label htmlFor='description'>Description</label>
+                            <input type='textarea' name='description' placeholder='Task Description'
+                                value={task.description} onChange={handleControlledInputChange} />
+                        </div>
+                    </fieldset>
             <button onClick={e => {
-            e.preventDefault()
-            taskId ? updateTask(task)
-            .then(() => history.push('/tasks')) 
-            : createNewTask(task)
-            .then(() => history.push('/tasks'))}}>Save Task</button>
+                e.preventDefault()
+                taskId ? updateTask(task)
+                    .then(() => history.push('/tasks'))
+                    : createNewTask(task)
+                        .then(() => history.push('/tasks'))
+            }}>Save Task</button>
+                </form>
+            </div>
+
         </>
     )
 }
